5b09df28659c9ada8bc113f8f1116f66540c42fc
Tammo Pape <tammo@elbstack.com>
2016-10-29 00:51:04 +0200

    update major versions from simple packages

--- src/components/CounterButton/CounterButton.js
+++ src/components/CounterButton/CounterButton.js
@@ -1,12 +1,12 @@
 import React, {Component, PropTypes} from 'react';
-import {connectMultireducer} from 'multireducer';
 import {increment} from 'redux/modules/counter';
 
-@connectMultireducer(
-  (key, state) => ({count: state.multireducer[key].count}),
-  {increment}
-)
 export default class CounterButton extends Component {
+
+  static defaultProps = {
+    increment: () => {}
+  }
+
   static propTypes = {
     count: PropTypes.number,
     increment: PropTypes.func.isRequired,
