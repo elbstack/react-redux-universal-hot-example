76298bb854977bdacb36bae6c1f1c0c2ec5e28ad
Tammo Pape <tammo@elbstack.com>
2016-10-29 00:36:29 +0200

    update minor / patch versions

--- src/client.js
+++ src/client.js
@@ -11,12 +11,12 @@ import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { ReduxAsyncConnect } from 'redux-async-connect';
-import useScroll from 'scroll-behavior/lib/useStandardScroll';
+import withScroll from 'scroll-behavior';
 
 import getRoutes from './routes';
 
 const client = new ApiClient();
-const _browserHistory = useScroll(() => browserHistory)();
+const _browserHistory = withScroll(browserHistory);
 const dest = document.getElementById('content');
 const store = createStore(_browserHistory, client, window.__data);
 const history = syncHistoryWithStore(_browserHistory, store);
